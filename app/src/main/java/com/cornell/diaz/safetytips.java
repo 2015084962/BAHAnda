eb30ceb16fc69308d6cb7d5abc5ad9c99638f16e
2015084962 <2015084962@ust-ics.mygbiz.com>
2018-10-23 15:47:46 +0800

    Initial commit

---
+++ app/src/main/java/com/cornell/diaz/safetytips.java
@@ -0,0 +1,29 @@
+package com.cornell.diaz;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class safetytips extends Fragment {
+
+
+    public safetytips() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_safetytips, container, false);
+    }
+
+}
